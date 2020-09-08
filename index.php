77e67eadf9d591f751c60f45a0880d613bca96dd
Ricardo Forte <rforte@redhat.com>
2020-09-08 16:47:09 -0300

    Cities listing

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
-header("Content-Type: text/plain");
+echo "<h1>Hello world</h1> ";
+echo "IP Interno: " . $_SERVER['SERVER_ADDR'];
+echo "<br>";
+
+$conn = new mysqli("mysql", "admin", "redhat@123", "clientdb");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$result = $conn->query("SELECT nome FROM cidade");
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo "<br>" . $row["nome"];
+    }
+} else {
+    echo " - 0 results";
+}
+$conn->close();
 ?>
-<?php if( $_ENV[ "HOSTNAME" ] ) { ?>PHP-<?php echo $_ENV["HOSTNAME"]; ?><?php } ?>
